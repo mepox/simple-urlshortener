4b50043697a9e7ad891d341eb435eb0e18dd2cb4
mepox <21198248+mepox@users.noreply.github.com>
2022-09-12 09:05:34 +0100

    add more tests

--- src/test/java/com/laszlojanku/spring/urlshortener/service/RandomKeyGeneratorServiceTest.java
+++ src/test/java/com/laszlojanku/spring/urlshortener/service/RandomKeyGeneratorServiceTest.java
@@ -1,5 +1,6 @@
 package com.laszlojanku.spring.urlshortener.service;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -13,12 +14,57 @@ public class RandomKeyGeneratorServiceTest {
 	private RandomKeyGeneratorService randomKeyGeneratorService;
 	
 	@Test
-	public void isValid_WhenValidKey_ShouldReturnTrue() {
-		String key = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	public void isValid_WhenKeyContainsNumbers_ShouldReturnTrue() {
+		String key = "1234";
 		
 		boolean actual = randomKeyGeneratorService.isValid(key);
 		
 		assertTrue(actual);
+	}
+	
+	@Test
+	public void isValid_WhenKeyContainsLowerCase_ShouldReturnTrue() {
+		String key = "abcd";
+		
+		boolean actual = randomKeyGeneratorService.isValid(key);
+		
+		assertTrue(actual);
+	}
+	
+	@Test
+	public void isValid_WhenKeyContainsUpperCase_ShouldReturnTrue() {
+		String key = "ABCD";
+		
+		boolean actual = randomKeyGeneratorService.isValid(key);
+		
+		assertTrue(actual);
+	}
+	
+	@Test
+	public void isValid_WhenKeyContainsAlphaNums_ShouldReturnTrue() {
+		String key = "a1B2";
+		
+		boolean actual = randomKeyGeneratorService.isValid(key);
+		
+		assertTrue(actual);
+	}
+	
+	@Test
+	public void isValid_WhenKeyIsEmpty_ShouldReturnFalse() {
+		String key = "";
+		
+		boolean actual = randomKeyGeneratorService.isValid(key);
+		
+		assertFalse(actual);
+	}
+	
+	@Test
+	public void isValid_WhenKeyNotInTheSeed_ShouldReturnFalse() {
+		String key = "a1B-";
+		
+		boolean actual = randomKeyGeneratorService.isValid(key);
+		
+		assertFalse(actual);
 	}
 
 }
---
+++ src/test/java/com/laszlojanku/spring/urlshortener/service/UrlValidatorServiceTest.java
@@ -0,0 +1,43 @@
+package com.laszlojanku.spring.urlshortener.service;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UrlValidatorServiceTest {
+
+	@InjectMocks
+	private UrlValidatorService urlValidatorService;
+	
+	@Test
+	public void isValid_WhenValidUrl_ShouldReturnTrue() {
+		String url = "http://www.google.com";
+		
+		boolean actual = urlValidatorService.isValid(url);
+		
+		assertTrue(actual);
+	}
+	
+	@Test
+	public void isValid_WhenHttpIsMissing_ShouldReturnFalse() {
+		String url = "www.google.com";
+		
+		boolean actual = urlValidatorService.isValid(url);
+		
+		assertFalse(actual);
+	}
+	
+	@Test
+	public void isValid_WhenOneWord_ShouldReturnFalse() {
+		String url = "google";
+		
+		boolean actual = urlValidatorService.isValid(url);
+		
+		assertFalse(actual);
+	}
+	
+}
