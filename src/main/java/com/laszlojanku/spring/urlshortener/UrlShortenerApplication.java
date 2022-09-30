882647c1a3a2489b84dac77b8fdd5b1c9736bf79
mepox <21198248+mepox@users.noreply.github.com>
2022-09-30 09:10:07 +0100

    change the way we instantiate the logger

--- src/main/java/com/laszlojanku/spring/urlshortener/UrlShortenerApplication.java
+++ src/main/java/com/laszlojanku/spring/urlshortener/UrlShortenerApplication.java
@@ -1,14 +1,10 @@
 package com.laszlojanku.spring.urlshortener;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UrlShortenerApplication {
-	
-	public static Logger logger = LoggerFactory.getLogger(UrlShortenerApplication.class);	
+public class UrlShortenerApplication {	
 
 	public static void main(String[] args) {
 		SpringApplication.run(UrlShortenerApplication.class, args);
--- src/main/java/com/laszlojanku/spring/urlshortener/service/StartupService.java
+++ src/main/java/com/laszlojanku/spring/urlshortener/service/StartupService.java
@@ -1,11 +1,11 @@
 package com.laszlojanku.spring.urlshortener.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
-
-import com.laszlojanku.spring.urlshortener.UrlShortenerApplication;
 
 /**
  * Run methods after the application started. 
@@ -13,19 +13,22 @@ import com.laszlojanku.spring.urlshortener.UrlShortenerApplication;
 @Service
 public class StartupService {
 	
+	private final Logger logger = LoggerFactory.getLogger(StartupService.class);
+	
 	@Autowired
 	private TinyURLService tinyURLService;
 	
 	@EventListener(ApplicationReadyEvent.class)
 	private void addSomeUrlAfterStartup() {
 		// Creates a few default TinyURLs
+		logger.info("Adding some default URLs...");
 		try {
 			tinyURLService.addURL("http://www.google.com");
 			tinyURLService.addURL("http://www.bing.com");
 			tinyURLService.addURL("http://www.yahoo.com");
 			tinyURLService.addURL("http://www.facebook.com");
 		} catch (Exception e) {
-			UrlShortenerApplication.logger.warn(e.getMessage());
+			logger.warn("Couldn't add the default URLs at startup: " + e.getMessage());
 		}
 	}
 
